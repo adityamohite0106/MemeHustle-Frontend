dc492acad9dabca95279a8c3b203614eadd7402a
adityamohite0106 <adityamohite4973@gmail.com>
2025-06-17 10:05:07 +0530

    new updates

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,8 @@
-// src/utils/socket.js
 import { io } from "socket.io-client";
 
-const BASE_URL = process.env.REACT_APP_API_URL; // from .env file
+const BASE_URL = process.env.REACT_APP_API_URL;
 console.log("Socket connecting to:", BASE_URL);
 
-export const socket = io(BASE_URL);
+export const socket = io(BASE_URL, {
+  transports: ["websocket"], // optional: improves stability
+});
